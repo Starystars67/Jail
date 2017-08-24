6b970f26ccd3ebb6714d4022c098ac99e09e15df
Starystars67 <starystars67@gmail.com>
2017-08-25 00:25:59 +0100

    Delete __resource.lua

--- Jail/__resource.lua
+++
@@ -1,9 +0,0 @@
--- ======================================
-      -- Written By : Titch2000
-	  -- Requested by NoamKvet
-      -- Free to use and edit but 
-	  -- please give credit
--- ======================================
-
-client_script 'jail_c.lua'
-server_script 'jail_s.lua'
